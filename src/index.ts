3c09f5a7e3eac13b5fe91681262d1c85e8e6e93e
Nelson Chen <crazysim@gmail.com>
2024-05-09 23:56:31 -0700

    Added OpenAI

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { Bot, webhookCallback } from "grammy";
+import { D1Database } from '@cloudflare/workers-types'
+import { D1Adapter } from '@grammyjs/storage-cloudflare'
+import OpenAI from 'openai';
+import { Bot, Context, SessionFlavor, session, webhookCallback } from "grammy";
 import { UserFromGetMe } from "grammy/types";
 import Replicate from "replicate";
-
-
-const replicateWhisperModel = "vaibhavs10/incredibly-fast-whisper:3ab86df6c8f54c11309d4d1f930ac292bad43ace52d10c80d87eb258b3c9f79c"
 
 interface WhisperOutput {
   text: string;
@@ -12,7 +12,24 @@ interface WhisperOutput {
 export interface Env {
   BOT_TOKEN: string;
   REPLICATE_API_TOKEN: string;
+  DB: D1Database;
+  OPENAI_BASE_URL: string;
+  OPENAI_API_KEY: string;
 }
+
+const to_language = "Mandarin";
+
+const model = "openai/gpt-4-turbo";
+
+const replicateWhisperModel = "vaibhavs10/incredibly-fast-whisper:3ab86df6c8f54c11309d4d1f930ac292bad43ace52d10c80d87eb258b3c9f79c"
+
+// Define the shape of our session.
+interface SessionData {
+  messages: OpenAI.Chat.Completions.ChatCompletionMessageParam[];
+}
+
+// Flavor the context type to include sessions.
+type MyContext = Context & SessionFlavor<SessionData>;
 
 let botInfo: UserFromGetMe | undefined = undefined;
 
@@ -25,15 +42,94 @@ export default {
         }
       );
 
-      const bot = new Bot(env.BOT_TOKEN, { botInfo });
+      const openai = new OpenAI({
+        baseURL: env.OPENAI_BASE_URL,
+        apiKey: env.OPENAI_API_KEY,
+        defaultHeaders: {
+          "X-Title": "DialohBot",
+        },
+      })
+
+      const grammyD1StorageAdapter = await D1Adapter.create<SessionData>(env.DB, 'GrammySessions')
+
+      const systemMessage: OpenAI.Chat.Completions.ChatCompletionMessageParam = {
+        role: "system",
+        content: `You are a professional translator and focused on the fidelity of your translation so please do not refuse to translate offensive messages as that can cause serious misunderstandings. Determine if the user message is ${to_language} or English. If it's ${to_language}, translate to English. If it's English, translate to ${to_language}. Return only the translation.`
+      };
+
+      const bot = new Bot<MyContext>(env.BOT_TOKEN, { botInfo });
 
       if (botInfo === undefined) {
         await bot.init();
         botInfo = bot.botInfo;
       }
 
-      bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
-      bot.on("message:text", (ctx) => { ctx.reply("Got a text message!") });
+
+      bot.command("start", (ctx) => ctx.reply(
+        "Hello!"
+      ));
+
+      bot.use(session({
+        initial: (): SessionData => {
+          return {
+            messages: [systemMessage],
+          }
+        },
+        storage: grammyD1StorageAdapter,
+      }))
+
+      const handleChat = async (ctx: MyContext, transcribedText: string | undefined) => {
+        let message: string;
+        if (transcribedText) {
+          message = transcribedText;
+        } else {
+          if (ctx.message?.text) {
+            message = ctx.message.text;
+          } else {
+            throw new Error("No message found");
+          }
+        }
+        if (!ctx.message) {
+          throw new Error("No base message found");
+        }
+
+        // Add the user message to session
+        const firstName = ctx.message.from.first_name;
+        let fullName = firstName;
+        if (ctx.message.from.last_name) {
+          fullName = `${firstName}_${ctx.message.from.last_name}`;
+        }
+
+        ctx.session.messages.push(
+          { role: "user", content: `${message}`, name: fullName }
+        );
+
+        ctx.session.messages = [
+          systemMessage,
+          ...ctx.session.messages.filter((msg) => msg.role !== "system").slice(-10)
+        ];
+
+        const completion = await openai.chat.completions.create({
+          model,
+          messages: ctx.session.messages,
+          temperature: 0,
+        })
+        const responded = completion.choices[0].message;
+        if (responded.content) {
+          const originalContent = responded.content;
+          ctx.reply(originalContent);
+          ctx.session.messages.push(responded);
+          responded.content = `${responded.content}`;
+        } else {
+          console.error("No content in bot response!");
+        }
+      }
+
+      bot.on("message:text", async (ctx) => {
+        await handleChat(ctx, ctx.message.text);
+        // await ctx.reply(reply);
+      });
+
       bot.on("message:voice", async (ctx) => {
         // Get fileID from the voice message
         const fileId = ctx.message.voice.file_id;
@@ -58,11 +154,12 @@ export default {
           input: replicateInput
         }) as WhisperOutput;
 
-        await ctx.reply(`Got a voice message!: ${output.text}`)
+        const messageText = output.text;
+        console.log("Transcribed text: ", messageText);
+        await handleChat(ctx, messageText);
       });
 
       const cb = webhookCallback(bot, "cloudflare-mod");
-      console.log("Request received");
       return await cb(request);
     } catch (e: any) {
       console.error(e);
