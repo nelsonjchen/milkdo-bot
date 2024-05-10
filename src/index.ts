a93d328c1f935f7c00ddec8d984696c4923c7b71
Nelson Chen <crazysim@gmail.com>
2024-05-09 21:12:34 -0700

    transcription test

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import { Bot, webhookCallback } from "grammy";
 import { UserFromGetMe } from "grammy/types";
+import Replicate from "replicate";
+
+
+const replicateWhisperModel = "vaibhavs10/incredibly-fast-whisper:3ab86df6c8f54c11309d4d1f930ac292bad43ace52d10c80d87eb258b3c9f79c"
+
+interface WhisperOutput {
+  text: string;
+}
 
 export interface Env {
   BOT_TOKEN: string;
+  REPLICATE_API_TOKEN: string;
 }
 
 let botInfo: UserFromGetMe | undefined = undefined;
@@ -10,6 +19,12 @@ let botInfo: UserFromGetMe | undefined = undefined;
 export default {
   async fetch(request: Request, env: Env) {
     try {
+      const replicate = new Replicate(
+        {
+          auth: env.REPLICATE_API_TOKEN,
+        }
+      );
+
       const bot = new Bot(env.BOT_TOKEN, { botInfo });
 
       if (botInfo === undefined) {
@@ -34,7 +49,16 @@ export default {
         if (file_path) {
           console.log(fullUrl);
         }
-        await ctx.reply(`Got a voice message!: ${file_path}`)
+        const replicateInput = {
+          audio: fullUrl,
+          batch_size: 64
+        };
+
+        const output = await replicate.run(replicateWhisperModel, {
+          input: replicateInput
+        }) as WhisperOutput;
+
+        await ctx.reply(`Got a voice message!: ${output.text}`)
       });
 
       const cb = webhookCallback(bot, "cloudflare-mod");
